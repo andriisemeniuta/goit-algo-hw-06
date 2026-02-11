77d2624c1f4246339acafffbb00ba674c529469f
Andrej <pytpoint@gmail.com>
2026-02-11 17:41:20 +0100

    try_1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,37 @@
 from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not (len(value) == 10 and value.isdigit()):
+            raise ValueError("Invalid phone format")
+
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    # реалізація класу
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    # реалізація класу
+    pass
