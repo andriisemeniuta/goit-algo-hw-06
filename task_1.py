9fb201cb563a3532e67ea6c9e762ccc191f95c6f
Andrej <pytpoint@gmail.com>
2026-02-12 21:08:00 +0100

    done

--- task_1.py
+++ task_1.py
@@ -26,12 +26,44 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    def add_phone(self, phone_number):
+        self.phones.append(Phone(phone_number))
+
+    def remove_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+                return
+            
+    def edit_phone(self, old_number, new_number):
+        for i, phone in enumerate(self.phones):
+            if phone.value == old_number:
+                self.phones[i] = Phone(new_number)
+                return
+        raise ValueError(("Phone not found"))
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+            return None
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 
 class AddressBook(UserDict):
-    # реалізація класу
-    pass
+    def add_record(self, record):
+        self.data[record.name.value] = record
+    
+    def find(self, name):
+        return self.data.get(name)
+    
+    def delete(self, name):
+        if name not in self.data:
+            raise ValueError("contact not found")
+        del self.data[name]
+
+
+
+    
